2518f12997f49ec476c9ba1f1c7b0bc141fe8cd3
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-12-28 11:51:55 -0500

    𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗛𝗘𝗣𝗘𝗜𝗡

--- scripts/no-recoil.user.js
+++ scripts/no-recoil.user.js
@@ -1,1 +1,21 @@
+// ==UserScript==
+// @name         Voxiom No Recoil
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Reducción experimental de retroceso
+// @author       Brashkie
+// @match        *://voxiom.io/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    Object.defineProperty(window, 'recoil', { value: 0, writable: false });
+    setInterval(() => {
+        if (window.game && window.game.player) {
+            window.game.player.recoil = 0;
+            window.game.player.weaponRecoil = 0;
+        }
+    }, 10);
+})();
