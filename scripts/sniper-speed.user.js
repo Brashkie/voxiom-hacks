5f18993486afd8a37315dcf137c78cdc16618afa
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-12-28 11:52:15 -0500

    𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗛𝗘𝗣𝗘𝗜𝗡

--- scripts/sniper-speed.user.js
+++ scripts/sniper-speed.user.js
@@ -1,1 +1,21 @@
+// ==UserScript==
+// @name         Voxiom Sniper Speed
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Movilidad mejorada con sniper
+// @author       Brashkie
+// @match        *://voxiom.io/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    setInterval(() => {
+        if (window.game && window.game.player && window.game.player.weapon) {
+            if (window.game.player.weapon.name.includes('Sniper')) {
+                window.game.player.speedMultiplier = 1.3;
+            }
+        }
+    }, 100);
+})();
