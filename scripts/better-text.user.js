89d0cb61eb5690f9c2e5db1777ecf31cf2ce63fe
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-12-28 11:52:32 -0500

    𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗛𝗘𝗣𝗘𝗜𝗡

--- scripts/better-text.user.js
+++ scripts/better-text.user.js
@@ -1,1 +1,26 @@
+// ==UserScript==
+// @name         Voxiom Better Text
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Mejora legibilidad del texto en pantalla
+// @author       Brashkie
+// @match        *://voxiom.io/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+        * {
+            text-shadow: 2px 2px 4px black !important;
+            font-weight: bold !important;
+            color: white !important;
+        }
+        .hud, .chat, .scoreboard {
+            font-size: 16px !important;
+        }
+    `;
+    document.head.appendChild(style);
+})();
