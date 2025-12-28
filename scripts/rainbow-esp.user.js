d595cb37ffc1f41fc92644241f5b688e8f35ede0
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-12-28 11:51:15 -0500

    𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗛𝗘𝗣𝗘𝗜𝗡

--- scripts/rainbow-esp.user.js
+++ scripts/rainbow-esp.user.js
@@ -1,1 +1,23 @@
+// ==UserScript==
+// @name         Voxiom Rainbow ESP
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Efecto visual multicolor en entidades (experimental)
+// @author       Brashkie
+// @match        *://voxiom.io/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    let hue = 0;
+
+    setInterval(() => {
+        hue = (hue + 1) % 360;
+        document.querySelectorAll('.player').forEach(player => {
+            player.style.outline = `3px solid hsl(${hue}, 100%, 50%)`;
+            player.style.boxShadow = `0 0 10px hsl(${hue}, 100%, 50%)`;
+        });
+    }, 50);
+})();
